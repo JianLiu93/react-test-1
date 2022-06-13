91dd9a0da47fe8351edabd692520b32413e313c5
JayLeon <coderjayleon@outlook.com>
2022-06-13 22:08:41 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom/client';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,9 +19,37 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div>{n}</div>
+      <button onClick={() => {
+        n += 1;
+        render();
+      }}
+      >
+      +1      
+      </button>
+      <div>组件
+        <Child numbers={['a','b','c']} />
+      </div>
     </div>
   );
 }
+
+let n = 0;
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+const render = () => {
+  root.render(<App />);
+}
+
+const Child = (props:any) => {
+  const array = []
+  for(let i=0;i<props.numbers.length;i++) {
+    array.push(<div>{i}: {props.numbers[i]}</div>)
+  }
+  return (<div>{ array }</div>)
+}
+
 
 export default App;
